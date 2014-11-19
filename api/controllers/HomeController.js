4806873b71695d25d993ef3c4e43ac8fd1cc771b
stormslowly <stormslowly@gmail.com>
2014-11-19 17:13:22 +0800

    add home page controller; .jshint

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,15 @@
+'use strict';
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+  index: function(req, res) {
+    return res.view('meetups', {
+          meetups: ['nokia', 'nodejs', 'python', 'lua', 'Golag']
+    });
+  },
+};
