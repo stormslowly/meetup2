e952b74405198b053d79dfd9274f5bb10d0a4a70
stormslowly <stormslowly@gmail.com>
2015-03-06 15:28:29 +0800

    auth finihsed

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,25 +8,32 @@
 
 module.exports = {
   index: function(req, res) {
+
+
     return res.view('meetups', {
-          meetups: ['nokia', 'nodejs', 'python', 'lua', 'Golag', 'Linux'], linkname: 'show'
+      meetups: ['nokia', 'nodejs', 'python', 'lua', 'Golag', 'Linux'],
+      linkname: 'show'
     });
   },
 
-  calender:function function_name (req,res) {
+  calender: function function_name(req, res) {
     return res.view('calender');
   },
 
-  show: function function_name (req, res) {
-  	console.log('come to showlayner');
-    Event.create({name:'WangGang'}, function(err, evt){
+  show: function function_name(req, res) {
+    console.log('come to showlayner');
+    Event.create({
+      name: 'WangGang'
+    }, function(err, evt) {
       if (err) console.log(err);
       console.log("database insert OK:" + JSON.stringify(evt));
     });
 
-    Event.find({}, function (err, events) {
+    Event.find({}, function(err, events) {
       console.log(events);
-      res.view('detail', {events:events});
+      res.view('detail', {
+        events: events
+      });
     });
 
   }
