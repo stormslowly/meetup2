3dd07573f300f5548bf363b3f2001f1006a07017
stormslowly <stormslowly@gmail.com>
2015-03-11 17:18:37 +0800

    format code

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -2,48 +2,47 @@
 
 module.exports = {
 
-    create: function(req, res) {
+  create: function(req, res) {
 
-        res.view('EventPublic', {title:'Event Public'});
-    },
+    res.view('EventPublic', {
+      title: 'Event Public'
+    });
+  },
 
-    show: function(req, res) {
-        console.log('come to showlayner');
+  show: function(req, res) {
 
-        Event.find({}, function(err, events) {
-            console.log(events);
-            res.view('detail', {
-                events: events,
-                layout: null
-            });
-        });
+    Event.find({}, function(err, events) {
+      console.log(events);
+      res.view('detail', {
+        events: events,
+        layout: null
+      });
+    });
 
-    },
+  },
 
-    createEvent: function(req, res) {
+  createEvent: function(req, res) {
 
-        console.log('create some event');
+    var newEvent = {
+      eventTopic: 'Clean Code Contest 2015',
+      eventDesc: ' Clean Code Contest',
+      eventOrganizer: 'Shu Pengfei',
+      eventAddress: 'Boston@16F',
+      eventDate: 2015 - 3 - 4
+    };
 
-        var newEvent = {
-            eventTopic: 'Clean Code Contest 2015',
-            eventDesc: ' Clean Code Contest',
-            eventOrganizer: 'Shu Pengfei',
-            eventAddress: 'Boston@16F',
-            eventDate: 2015 - 3 - 4
-        };
+    Event.create(newEvent, function(err, evt) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Event create successfully' + JSON.stringify(evt));
+      res.view('EventCreated', {
+        events: evt,
+        layout: null
+      });
 
-        Event.create(newEvent, function(err, evt) {
-            if (err) {
-                console.log(err);
-            }
-            console.log('Event create successfully' + JSON.stringify(evt));
-            res.view('EventCreated', {
-                events: evt,
-                layout: null
-            });
+    });
 
-        });
-
-    }
+  }
 
 };
