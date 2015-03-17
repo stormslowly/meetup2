1669cb2a4cc38ea7567da3ade2f84ef349221c50
Wang Gang <gang-layner.wang@nsn.com>
2015-03-17 15:53:13 +0800

    redirect to detail page once event created

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -5,35 +5,50 @@ module.exports = {
 
   create: function(req, res) {
 
-    res.view('EventPublic', {
-      title: 'Event Public'
+    Group.find({}, function(err, found) {
+      if (err) {
+        console.log('Something is wrong:', err);
+        return res.negotiate(err);
+      }
+
+      res.view('EventPublic', {
+        title: 'Event Public',
+        groups: found
+      });
+
     });
+
+
   },
 
   show: function(req, res) {
-    var strPath= req.path;
+    var strPath = req.path;
     var pos1 = strPath.indexOf('/show/') + 6;
     var pos2 = strPath.length;
-    var eventId= strPath.slice(pos1, pos2);
+    var eventId = strPath.slice(pos1, pos2);
 
     console.log("eventId is:", eventId);
 
-    Event.find({id: eventId}, function(err, events) {
+    Event.find({
+      id: eventId
+    }, function(err, events) {
       console.log(events);
       var eve = new Object();
       eve = events[0];
       var groupid = eve.group;
-      
-      Group.find({id:groupid}, function(err,groups){
+
+      Group.find({
+        id: groupid
+      }, function(err, groups) {
         var gro = new Object();
         gro = groups[0];
         res.view('detail', {
-        eventobj: eve,
-        groupobj: gro,
-        layout: null
+          eventobj: eve,
+          groupobj: gro,
+          layout: null
         });
-      });  
-      
+      });
+
     });
 
   },
@@ -46,15 +61,33 @@ module.exports = {
     newEvent.desc = req.param('Event');
     newEvent.date = req.param('Date');
     newEvent.address = req.param('Address');
-    newEvent.group = req.param('Group');
-    console.log(newEvent.group);
+    var groupname = req.param('Group');
+    console.log('group name:', groupname);
 
-    Event.create(newEvent, function(err) {
+    Group.find({
+      name: groupname
+    }, function(err, found) {
       if (err) {
         sails.log.error(err);
         return res.negotiate(err);
       }
-      res.redirect(200, '/calender');
+
+      var groupid = found[0].id;
+      console.log('groupis is: ', groupid);
+      newEvent.group = groupid;
+
+      Event.create(newEvent, function(err, created) {
+        if (err) {
+          sails.log.error(err);
+          return res.negotiate(err);
+        }
+        var linkid = '/show/' + created.id;
+        res.redirect(linkid);
+      });
+
     });
+
+
   }
+  
 };
